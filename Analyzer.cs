53081e2d4517edff9e4c8cc50dbf0c6d883546e9
Kuran Kaname <celtare21@gmail.com>
2023-01-18 12:41:26 +0200

    Initial commit
    Signed-off-by: Kuran Kaname <celtare21@gmail.com>

---
+++ Analyzer.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Analyzer;
+
+public static class LexicalAnalyzer
+{
+    public static void Analyze()
+    {
+        {
+            //Read C++ code from file
+            string code = File.ReadAllText("code.cpp");
+
+            // Create a SyntaxTree from the code
+            var tree = CSharpSyntaxTree.ParseText(code);
+
+            // Get the root node of the syntax tree
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+
+            // Extract variable and function declarations
+            var variableDeclarations = root.DescendantNodes().OfType<VariableDeclarationSyntax>();
+            var functionDeclarations = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+            // Extract all the tokens including signs like "+","-","=", etc
+            var tokens = root.DescendantTokens();
+
+            var signs = (from token in tokens
+                where token.IsKind(SyntaxKind.PlusToken) || token.IsKind(SyntaxKind.MinusToken) || token.IsKind(SyntaxKind.EqualsToken) ||
+                      token.IsKind(SyntaxKind.AsteriskToken) || token.IsKind(SyntaxKind.SlashToken) || token.IsKind(SyntaxKind.PercentToken) ||
+                      token.IsKind(SyntaxKind.AmpersandToken) || token.IsKind(SyntaxKind.BarToken) || token.IsKind(SyntaxKind.CaretToken) ||
+                      token.IsKind(SyntaxKind.TildeToken) || token.IsKind(SyntaxKind.ExclamationToken) || token.IsKind(SyntaxKind.LessThanToken) ||
+                      token.IsKind(SyntaxKind.GreaterThanToken) || token.IsKind(SyntaxKind.AmpersandAmpersandToken) || token.IsKind(SyntaxKind.BarBarToken) ||
+                      token.IsKind(SyntaxKind.QuestionToken) || token.IsKind(SyntaxKind.ColonToken) || token.IsKind(SyntaxKind.SemicolonToken) ||
+                      token.IsKind(SyntaxKind.CommaToken) || token.IsKind(SyntaxKind.DotToken) || token.IsKind(SyntaxKind.DotDotToken) ||
+                      token.IsKind(SyntaxKind.PlusPlusToken) || token.IsKind(SyntaxKind.MinusMinusToken) || token.IsKind(SyntaxKind.PercentEqualsToken) ||
+                      token.IsKind(SyntaxKind.LessThanLessThanToken) || token.IsKind(SyntaxKind.GreaterThanGreaterThanToken) ||
+                      token.IsKind(SyntaxKind.LessThanEqualsToken) || token.IsKind(SyntaxKind.GreaterThanEqualsToken) || token.IsKind(SyntaxKind.EqualsEqualsToken) ||
+                      token.IsKind(SyntaxKind.ExclamationEqualsToken) || token.IsKind(SyntaxKind.AmpersandEqualsToken) || token.IsKind(SyntaxKind.BarEqualsToken) ||
+                      token.IsKind(SyntaxKind.CaretEqualsToken) || token.IsKind(SyntaxKind.LessThanLessThanEqualsToken) ||
+                      token.IsKind(SyntaxKind.GreaterThanGreaterThanEqualsToken) || token.IsKind(SyntaxKind.SlashEqualsToken) ||
+                      token.IsKind(SyntaxKind.AsteriskEqualsToken) || token.IsKind(SyntaxKind.PlusEqualsToken) || token.IsKind(SyntaxKind.MinusEqualsToken)
+                select token.ValueText).ToList();
+
+            Console.WriteLine("Signs:");
+            foreach (var sign in signs)
+            {
+                Console.Write($"{sign}   ");
+            }
+
+            // Output variable names, data types, and values
+            Console.WriteLine("\n\nVariable Declarations:\n");
+            foreach (var variableDeclaration in variableDeclarations)
+            {
+                var type = variableDeclaration.Type;
+                var variables = variableDeclaration.Variables;
+                foreach (var variable in variables)
+                {
+                    Console.WriteLine("Name: " + variable.Identifier.ValueText);
+                    Console.WriteLine("Data Type: " + type);
+                    if (variable.Initializer != null)
+                        Console.WriteLine("Value: " + variable.Initializer.Value);
+                }
+
+                Console.WriteLine();
+            }
+
+            // Output function names, return data types, parameter names, and data types
+            Console.WriteLine("\nFunction Declarations:\n");
+            foreach (var functionDeclaration in functionDeclarations)
+            {
+                var returnType = functionDeclaration.ReturnType;
+                var name = functionDeclaration.Identifier.ValueText;
+                var parameters = functionDeclaration.ParameterList.Parameters;
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Return Type: " + returnType);
+                foreach (var parameter in parameters)
+                {
+                    Console.WriteLine("Parameter Name: " + parameter.Identifier.ValueText);
+                    Console.WriteLine("Data Type: " + parameter.Type);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ code.cpp
@@ -0,0 +1,148 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Function to add two numbers
+double add(double num1, double num2)
+{
+    return num1 + num2;
+}
+
+// Function to subtract two numbers
+double subtract(double num1, double num2)
+{
+    return num1 - num2;
+}
+
+// Function to multiply two numbers
+double multiply(double num1, double num2)
+{
+    return num1 * num2;
+}
+
+// Function to divide two numbers
+double divide(double num1, double num2)
+{
+    if (num2 == 0)
+    {
+        cout << "Error: Cannot divide by zero." << endl;
+        return 0;
+    }
+
+    return num1 / num2;
+}
+
+// Function to calculate the square of a number
+double square(double num)
+{
+    return num * num;
+}
+
+// Function to calculate the square root of a number
+double square_root(double num)
+{
+    if (num < 0)
+    {
+        cout << "Error: Cannot calculate the square root of a negative number." << endl;
+        return 0;
+    }
+
+    return sqrt(num);
+}
+
+// Function to calculate the power of a number
+double power(double num, double exponent)
+{
+    return pow(num, exponent);
+}
+
+int main()
+{
+    double num1, num2;
+    char operation;
+    bool is_float = false;
+    bool run_calculator = true;
+
+    while (run_calculator)
+    {
+        // Ask the user if they want to perform calculations with floating point numbers
+        cout << "Do you want to perform calculations with floating point numbers? (y/n): ";
+        char choice;
+        cin >> choice;
+        if (choice == 'y' || choice == 'Y')
+            is_float = true;
+
+        // Display menu of options to the user
+        cout << "Select an operation:" << endl;
+        cout << "1. Addition" << endl;
+        cout << "2. Subtraction" << endl;
+        cout << "3. Multiplication" << endl;
+        cout << "4. Division" << endl;
+        cout << "5. Square" << endl;
+        cout << "6. Square Root" << endl;
+        cout << "7. Power" << endl;
+        cout << "8. Exit" << endl;
+        cin >> operation;
+
+        // Get the numbers from the user
+        if (operation != '8')
+        {
+            cout << "Enter the first number: ";
+            if (is_float)
+                cin >> num1;
+            else
+            {
+                int temp;
+                cin >> temp;
+                num1 = temp;
+            }
+
+            if (operation != '5' && operation != '6')
+            {
+                cout << "Enter the second number: ";
+                if (is_float)
+                    cin >> num2;
+                else
+                {
+                    int temp;
+                    cin >> temp;
+                    num2 = temp;
+                }
+            }
+        }
+
+        // Perform the appropriate calculation based on the operation entered
+        switch (operation)
+        {
+            case '1':
+                cout << num1 << " + " << num2 << " = " << add(num1, num2) << endl;
+                break;
+            case '2':
+                cout << num1 << " - " << num2 << " = " << subtract(num1, num2) << endl;
+                break;
+            case '3':
+                cout << num1 << " * " << num2 << " = " << multiply(num1, num2) << endl;
+                break;
+            case '4':
+                cout << num1 << " / " << num2 << " = " << divide(num1, num2) << endl;
+                break;
+            case '5':
+                cout << "Square of " << num1 << " = " << square(num1) << endl;
+                break;
+            case '6':
+                cout << "Square root of " << num1 << " = " << square_root(num1) << endl;
+                break;
+            case '7':
+                cout << num1 << " raised to the power of " << num2 << " = " << power(num1, num2) << endl;
+                break;
+            case '8':
+                cout << "Exiting Calculator..." << endl;
+                run_calculator = false;
+                break;
+            default:
+                cout << "Error: Invalid selection. Please enter a valid option." << endl;
+        }
+    }
+
+    return 0;
+}
